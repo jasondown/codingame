bcfcd00a38d37ca63f323adf3d31766c1bfb9942
Jason Down <jdown@igloosoftware.com>
2018-01-15 22:12:13 -0500

    Added onboarding C# solution.

---
+++ csharp/Onboarding.cs
@@ -0,0 +1,26 @@
+﻿// https://www.codingame.com/training/easy/onboarding
+
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+class Player
+{
+    static void Main(string[] args)
+    {
+
+        // game loop
+        while (true)
+        {
+            string enemy1 = Console.ReadLine(); 
+            int dist1 = int.Parse(Console.ReadLine());
+            string enemy2 = Console.ReadLine();
+            int dist2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(dist1 < dist2 ? enemy1 : enemy2);
+        }
+    }
+}
