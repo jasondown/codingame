29c0567b834862c005374232fa4dcf2ed4e3206c
Jason Down <jdown@igloosoftware.com>
2018-01-16 23:30:39 -0500

    Added onboarding solution for Scala.

---
+++ scala/classic_puzzles_easy/Onboarding.scala
@@ -0,0 +1,18 @@
+// https://www.codingame.com/training/easy/onboarding
+
+import math._
+import scala.util._
+
+object Player extends App {
+
+    // game loop
+    while(true) {
+        val enemy1 = readLine // name of enemy 1
+        val dist1 = readInt // distance to enemy 1
+        val enemy2 = readLine // name of enemy 2
+        val dist2 = readInt // distance to enemy 2
+        
+
+        println(if (dist1 < dist2) enemy1 else enemy2)
+    }
+}
