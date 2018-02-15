c8c7dcb4977000dd1734b2fa8bc9f49f43584347
Jason Down <jdown@igloosoftware.com>
2018-02-14 20:58:26 -0500

    Added graph partial active pattern and ability to get edges of a graph.

--- fsharp/classic_puzzles_hard/scratch_pad.fsx
+++ fsharp/classic_puzzles_hard/scratch_pad.fsx
@@ -6,3 +6,15 @@ type Vertex =
 type Edge = (Vertex * Vertex) * double
 
 type Graph = Set<Vertex> * Set<Vertex>
+
+type AdjacencyList<'TLabel when 'TLabel : comparison> = 
+    L of Map<Vertex, Set<(Vertex * 'TLabel)>>
+
+// May support directed vs undirected later
+let (|Graph|_|) graph = 
+    match graph with
+    |  (v, e)   -> Some (v, e)
+
+let getEdges = function 
+    | Graph (_, e) -> e
+    | x -> failwithf "Not a graph: %A" x
