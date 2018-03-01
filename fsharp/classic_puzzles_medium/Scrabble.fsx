6f3ce061a9b8fca0fd4b1c2246f7f2250633f7fd
Jason Down <jdown@igloosoftware.com>
2018-02-28 22:43:17 -0500

    F# scrabble - Get words and letters from input.

--- fsharp/classic_puzzles_medium/Scrabble.fsx
+++ fsharp/classic_puzzles_medium/Scrabble.fsx
@@ -1,5 +1,7 @@
 // https://www.codingame.com/training/medium/scrabble
 
+open System
+
 let pointValues = 
     dict ['e',1; 'a',1; 'i',1; 'o',1; 'n',1; 'r',1; 't',1; 'l',1; 's',1; 'u',1;
           'd',2; 'g',2;
@@ -10,3 +12,17 @@ let pointValues =
           'q',10; 'z',10; 
     ]
 
+let readNLines n = Array.init n (fun _ -> Console.ReadLine())
+
+let getWordsAndLetters wordCount =
+    let words = readNLines (wordCount - 1)
+    let letters = readNLines 1 |> Array.head
+    let validWords =
+        words
+        |> Array.filter (fun w -> w.Length <= 7)
+    (validWords, letters)
+
+let words, letters = 
+    Console.ReadLine()
+    |> int
+    |> getWordsAndLetters
