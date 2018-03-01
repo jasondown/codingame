17908408fbf10691da07a9698f262ae9c127d6e5
Jason Down <jdown@igloosoftware.com>
2018-02-28 22:32:07 -0500

    Began adding F# solution for scrabble (classic medium puzzle).

---
+++ fsharp/classic_puzzles_medium/Scrabble.fsx
@@ -0,0 +1,12 @@
+// https://www.codingame.com/training/medium/scrabble
+
+let pointValues = 
+    dict ['e',1; 'a',1; 'i',1; 'o',1; 'n',1; 'r',1; 't',1; 'l',1; 's',1; 'u',1;
+          'd',2; 'g',2;
+          'b',3; 'c',3; 'm',3; 'p',3;
+          'f',4; 'h',4; 'v',4; 'w',4; 'y',4;
+          'k',5;
+          'j',8; 'x',8;
+          'q',10; 'z',10; 
+    ]
+
