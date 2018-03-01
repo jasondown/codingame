60e7c1deed2c79436aff4e81c9f56e4cf7998849
Jason Down <jdown@igloosoftware.com>
2018-03-01 16:17:50 -0500

    Finshed scrabble solution for F#.

--- fsharp/classic_puzzles_medium/Scrabble.fsx
+++ fsharp/classic_puzzles_medium/Scrabble.fsx
@@ -1,6 +1,7 @@
 // https://www.codingame.com/training/medium/scrabble
 
 open System
+open System.Collections.Generic
 
 let pointValues = 
     dict ['e',1; 'a',1; 'i',1; 'o',1; 'n',1; 'r',1; 't',1; 'l',1; 's',1; 'u',1;
@@ -12,17 +13,29 @@ let pointValues =
           'q',10; 'z',10; 
     ]
 
-let readNLines n = Array.init n (fun _ -> Console.ReadLine())
+let getHash (word : string) =
+    let count c = Seq.filter ((=) c) >> Seq.length
+    word |> Seq.distinct |> Seq.map (fun c -> c, (word |> count c)) |> dict
 
-let getWordsAndLetters wordCount =
-    let words = readNLines wordCount
-    let letters = readNLines 1 |> Array.head
-    let validWords =
-        words
-        |> Array.filter (fun w -> w.Length <= 7)
-    (validWords, letters)
+let getScore (points : IDictionary<char, int>) (letters : IDictionary<char, int>) word =
+    let mutable total = 0
+    let wordHash = getHash word
+    for kvp in wordHash do
+        let c = kvp.Key
+        if (total = -1 || not <| letters.ContainsKey c || letters.[c] < wordHash.[c])
+        then total <- -1
+        else total <- total + wordHash.[c] * points.[c]
+    total
 
-let words, letters = 
-    Console.ReadLine()
-    |> int
-    |> getWordsAndLetters
+let getBestWord points letters words =
+    words |> List.maxBy (fun w -> getScore points letters w)
+
+let getLettersAndWords wordCount =
+    let validWords = 
+        List.init wordCount (fun _ -> Console.ReadLine())
+        |> List.filter (fun w -> w.Length <= 7)
+    let letters = Console.ReadLine() |> getHash
+    (letters, validWords)
+
+//----------- main logic     
+Console.ReadLine() |> int |> getLettersAndWords ||> getBestWord pointValues |> printfn "%s" 
