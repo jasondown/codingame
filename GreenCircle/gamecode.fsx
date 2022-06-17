11e8b12a19dd8d0ce99bfd0c99a1325a8d55c90c
Jason Down <rem_idle@hotmail.com>
2022-06-17 01:27:10 -0400

    Passed Wood League 2

--- GreenCircle/gamecode.fsx
+++ GreenCircle/gamecode.fsx
@@ -32,14 +32,30 @@ type Skill =
         | 7 -> Refactoring
         | x -> failwithf "Unknown Skill: %i" x
 
-type DeskLocation = 
+    override this.ToString() =
+        match this with
+        | Training -> "0"
+        | Coding -> "1"
+        | DailyRoutine -> "2"
+        | TaskPrioritization -> "3"
+        | ArchitectureStudy -> "4"
+        | ContinuousIntegration -> "5"
+        | CodeReview -> "6"
+        | Refactoring -> "7"
+
+type DeskLocation =
     | None
     | Skill of Skill
     static member Create =
         function
         | -1 -> None
-        | x when x >= 0 && x <= 7 -> Skill (Skill.Create x)
+        | x when x >= 0 && x <= 7 -> Skill(Skill.Create x)
         | x -> failwithf "Unknown Desk Location: %i" x
+
+    override this.ToString() =
+        match this with
+        | Skill s -> s.ToString()
+        | None -> ""
 
 type CardType =
     | Skill of Skill
@@ -51,8 +67,8 @@ type CardLocation =
     | Draw
     | Discard
     | OpponentCards
-    static member Create = 
-        function 
+    static member Create =
+        function
         | "HAND" -> Hand
         | "DRAW" -> Draw
         | "DISCARD" -> Discard
@@ -60,45 +76,36 @@ type CardLocation =
         | x -> failwithf "Unknown Card Location: %s" x
 
 type Card =
-    {
-        Location: CardLocation
-        Type: CardType
-    }
+    { Location: CardLocation
+      Type: CardType }
 
 type Application =
-    {
-        Id: int
-        RequiredSkills: Map<Skill, int>
-    }
-    static member Create (token: Token) =
-        {
-            Id = int token.[1]
-            RequiredSkills = 
-                Map.empty
-                    .Add(Training, int token.[2])
-                    .Add(Coding, int token.[3])
-                    .Add(DailyRoutine, int token.[4])
-                    .Add(TaskPrioritization, int token.[5])
-                    .Add(ArchitectureStudy, int token.[6])
-                    .Add(ContinuousIntegration, int token.[7])
-                    .Add(CodeReview, int token.[8])
-                    .Add(Refactoring, int token.[9])
-        }
+    { Id: int
+      RequiredSkills: Map<Skill, int> }
+    static member Create(token: Token) =
+        { Id = int token.[1]
+          RequiredSkills =
+            Map
+                .empty
+                .Add(Training, int token.[2])
+                .Add(Coding, int token.[3])
+                .Add(DailyRoutine, int token.[4])
+                .Add(TaskPrioritization, int token.[5])
+                .Add(ArchitectureStudy, int token.[6])
+                .Add(ContinuousIntegration, int token.[7])
+                .Add(CodeReview, int token.[8])
+                .Add(Refactoring, int token.[9]) }
 
 type Player =
-    {
-        Location: DeskLocation
-        ReleasedApps: int
-        PermanentDailyRoutineCards: int // ignore for Wood 2
-        PermanentArchitectureStudyCards: int // ignore for Wood 2
-    }
-    static member Create (token: Token) =
-        {
-            Location = DeskLocation.Create (int token.[0])
-            ReleasedApps = int token.[1]
-            PermanentDailyRoutineCards = int token.[2]
-            PermanentArchitectureStudyCards = int token.[3]
-        }
+    { Location: DeskLocation
+      ReleasedApps: int
+      PermanentDailyRoutineCards: int // ignore for Wood 2
+      PermanentArchitectureStudyCards: int } // ignore for Wood 2
+    static member Create(token: Token) =
+        { Location = DeskLocation.Create(int token.[0])
+          ReleasedApps = int token.[1]
+          PermanentDailyRoutineCards = int token.[2]
+          PermanentArchitectureStudyCards = int token.[3] }
 
 type Move =
     | Move of int
@@ -111,97 +118,156 @@ type Move =
         | Release i -> sprintf "RELEASE %i" i
         | Random -> sprintf "RANDOM"
         | Wait -> sprintf "WAIT"
-    static member Create (s: string) =
+
+    static member Create(s: string) =
         match s.Split ' ' |> Array.toList with
-        | "MOVE" :: i :: _ -> Move (int i)
-        | "RELEASE" :: i :: _ -> Release (int i)
+        | "MOVE" :: i :: _ -> Move(int i)
+        | "RELEASE" :: i :: _ -> Release(int i)
         | "WAIT" :: _ -> Wait
         | "RANDOM" :: _ -> Random
         | x -> failwithf "Unrecognized Move: %A" x
 
-type Gamestate = 
-    {
-        Phase: GamePhase
-        Me: Player
-        Opponent: Player
-        Applications: Application list
-        Cards: (Card * int) list
-        AvailMoves: Move list
-    }
+type Gamestate =
+    { Phase: GamePhase
+      Me: Player
+      Opponent: Player
+      Applications: Application list
+      Cards: (Card * int) list
+      AvailMoves: Move list }
 
 let readInput () = Console.In.ReadLine()
 let readInt = readInput >> int
-let tokenize (line : string) : Token = line.Split ' '
+let tokenize (line: string) : Token = line.Split ' '
 let tokenizeInput = readInput >> tokenize
-let readNLines n = List.init n (fun _ -> readInput())
+let readNLines n = List.init n (fun _ -> readInput ())
 
 let createCards (token: Token) =
     let location = CardLocation.Create token.[0]
+
     let skillCards =
         token.[1..8]
-        |> Array.mapi (fun i n -> CardType.Skill (Skill.Create i), int n)
-        |> Array.map (fun (s, n) -> { Card.Location = location; Card.Type = s }, n)
+        |> Array.mapi (fun i n -> CardType.Skill(Skill.Create i), int n)
+        |> Array.map (fun (s, n) ->
+            { Card.Location = location
+              Card.Type = s },
+            n)
         |> List.ofArray
-    let bonusCards = { Card.Location = location; Card.Type = Bonus}, (int token.[9])
-    let techDebtCards = { Card.Location = location; Card.Type = TechnicalDebt}, (int token.[10]    )
-    techDebtCards :: bonusCards :: skillCards |> List.rev
+
+    let bonusCards =
+        { Card.Location = location
+          Card.Type = Bonus },
+        (int token.[9])
+
+    let techDebtCards =
+        { Card.Location = location
+          Card.Type = TechnicalDebt },
+        (int token.[10])
+
+    techDebtCards :: bonusCards :: skillCards
+    |> List.rev
+
+let getTargetApp (gs: Gamestate) (myCards: (Card * int) list) =
+    let apps = gs.Applications
+
+    let requiredSkills =
+        apps
+        |> List.map (fun a ->
+            let rs = a.RequiredSkills |> Map.filter (fun _ i -> i > 0)
+            { Id = a.Id; RequiredSkills = rs })
+
+    let getScore (rs: Map<Skill, int>) (cards: Card list) =
+        let rec score (cardList: Card list) acc =
+            match cardList with
+            | [] -> acc
+            | c :: cs ->
+                match c.Type with
+                | Skill s ->
+                    if rs.ContainsKey(s) then
+                        score cs (acc + 10)
+                    else
+                        score cs acc
+                | Bonus -> score cs (acc + 1)
+                | TechnicalDebt -> score cs acc
+
+        score cards 0
+
+    let targetApp =
+        requiredSkills
+        |> List.sortByDescending (fun rs -> getScore rs.RequiredSkills (myCards |> List.map fst))
+        |> List.head
+
+    targetApp
+
+let getTargetDesk (gs: Gamestate) (targetApp: Application) (myCards: (Card * int) list) =
+    let skillCards =
+        myCards
+        |> List.map (fun (c, n) -> c.Type, n)
+        |> List.choose (fun (c, _) ->
+            match c with
+            | Skill s -> Some s
+            | _ -> Option.None)
+
+    let x =
+        targetApp.RequiredSkills
+        |> Map.filter (fun sk _ -> skillCards |> List.exists (fun s -> s = sk) |> not)
+        |> Seq.tryHead
+        |> Option.defaultValue (Collections.Generic.KeyValuePair.Create(Training, 0))
+
+    DeskLocation.Skill x.Key
+
 
 let getMove (gs: Gamestate) : Move =
-    gs.AvailMoves.Head
 
-    // let myCards =
-    //     cards |> List.filter (fun c -> 
-    //         let card = fst c
-    //         card.Location = Hand)
+    let myCards =
+        gs.Cards
+        |> List.filter (fun c ->
+            let card = fst c
+            card.Location = Hand && (snd c) > 0)
 
-    // let drawCards = 
-    //     cards |> List.filter (fun c ->
-    //         let card = fst c
-    //         card.Location = Draw)
+    let targetApp = getTargetApp gs myCards
 
-    // let discardCards =
-    //     cards |> List.filter (fun c ->
-    //         let card = fst c
-    //         card.Location = Discard)
+    let canMake =
+        gs.AvailMoves
+        |> List.exists (fun am ->
+            match am with
+            | Release i when i = targetApp.Id -> true
+            | _ -> false)
 
-    // let oppCards =
-    //     cards |> List.filter (fun c ->
-    //         let card = fst c
-    //         card.Location = OpponentCards)
+    if canMake then
+        Release targetApp.Id
+    else
+        let targetDesk = getTargetDesk gs targetApp myCards
+
+        if targetDesk <> gs.Me.Location then
+            Move(int <| targetDesk.ToString())
+        else
+            Random
 
 while true do
-    let gamePhase = readInput() |> GamePhase.Create
+    let gamePhase = readInput () |> GamePhase.Create
 
     let apps =
-        readInt()
+        readInt ()
         |> readNLines
         |> List.map (tokenize >> Application.Create)
 
-    let myInfo = tokenizeInput() |> Player.Create
+    let myInfo = tokenizeInput () |> Player.Create
 
-    let oppInfo = tokenizeInput() |> Player.Create
+    let oppInfo = tokenizeInput () |> Player.Create
 
     let cards =
-        readInt()
+        readInt ()
         |> readNLines
         |> List.collect (tokenize >> createCards)
 
-    let possibleMoves =
-        readInt()
-        |> readNLines
-        |> List.map Move.Create
+    let possibleMoves = readInt () |> readNLines |> List.map Move.Create
 
-    let gameState = 
-        {
-            Phase = gamePhase
-            Me = myInfo
-            Opponent = oppInfo
-            Applications = apps
-            Cards = cards
-            AvailMoves = possibleMoves
-        }
+    let gameState =
+        { Phase = gamePhase
+          Me = myInfo
+          Opponent = oppInfo
+          Applications = apps
+          Cards = cards
+          AvailMoves = possibleMoves }
 
-    gameState
-    |> getMove
-    |> string
-    |> printfn "%s" 
+    gameState |> getMove |> string |> printfn "%s"
